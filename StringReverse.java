796c3a696a6573e67602ab70a19997518007989d
kps11 <kpsenapati6@gmail.com>
2024-06-10 07:14:51 +0530

    leetcode 145

---
+++ StringReverse.java
@@ -0,0 +1,24 @@
+package java_pratice.array_string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        String s = "    The sky is  blue   ";
+        String [] array_string = s.split(" ");
+        ArrayList <String> al = new ArrayList<>();
+        for (int i = 0; i < array_string.length; i++) {
+            if (array_string[i] !="") {
+
+                al.add(array_string[i]);
+            }
+        }
+        String result = String.join(" ", al.reversed());
+
+
+
+        System.out.print(result);
+    }
+}
